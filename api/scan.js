11d26e295d74206207c3150b7a3f907aaa037768
anaelfameni <anaelfameni@gmail.com>
2026-05-04 00:00:29 +0000

    fix-allowed-origins-scan-api

--- api/scan.js
+++ api/scan.js
@@ -674,13 +674,7 @@ function buildCriticalAlerts(sec, ux, perf) {
 
 
 export default async function handler(req, res) {
-    const origin = req.headers.origin || '';
-    if (ALLOWED_ORIGINS.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-User-Id, x-user-id');
+    setCorsHeaders(req, res);
 
     if (req.method === 'OPTIONS') return res.status(200).end();
     if (req.method !== 'POST')
